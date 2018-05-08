2e9ace9700f2d70abf4822306ad080716dd74109
littleyellow <450047998@qq.com>
2018-05-09 00:14:58 +0800

    payhelper->1.0.1

--- payhelper/src/main/java/com/littleyellow/payhelper/GlobalPayInfo.java
+++ payhelper/src/main/java/com/littleyellow/payhelper/GlobalPayInfo.java
@@ -27,6 +27,9 @@ public abstract class GlobalPayInfo {
 
     public abstract String getAliNotifyUrl();
 
+    //      0	成功	展示成功页面
+    //      -1	错误	可能的原因：签名错误、未注册APPID、项目设置APPID不正确、注册的APPID与设置的不匹配、其他异常等。
+    //      -2	用户取消	无需处理。发生场景：用户不支付了，点击取消，返回APP。
     public void onResp(Context context,BaseResp resp){
         try {
             if(null!= WXPay.WXPayListener){
